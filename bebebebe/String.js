1b3c365a8ff0909f2d57c7d8c53c319d49711d0d
jorgecostasouza-sys <jorge.costa.souza@escola.pr.gov.br>
2026-03-12 08:31:35 -0300

    Add files via upload

---
+++ bebebebe/String.js
@@ -0,0 +1,9 @@
+//exemplos de texto
+let nome = "Shirose"
+let sobrenome = "Prado"
+let nomeCompleto = nome + " " + sobrenome
+//concatenação das variaveis
+console.log(nomeCompleto)
+let senha ='9393'
+let stringDeNumeros = '123456789'
+console.log(stringDeNumeros+2)
