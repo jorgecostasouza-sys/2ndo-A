1297329d90b66aefdd59884ea6a019b051915a2c
jorgecostasouza-sys <jorge.costa.souza@escola.pr.gov.br>
2026-03-18 10:05:11 -0300

    Add files via upload

---
+++ mibombo~javascript/bebebebe/s.js
@@ -0,0 +1,12 @@
+var altura = 5;
+var comprimento = 7;
+let area;
+let forma 'retângulo'
+
+if(forma === 'retângulo'){
+    area = altura * comprimento
+}else {
+    area = (altura * comprimento) / 2;
+}
+area = altura * comprimento;
+console.log(area)
